4e0fdc27b03be69b4148ccda00cd5dcde38605f1
LudaShersh <luda.shershnyova@gmail.com>
2023-01-26 02:18:03 +0400

    feat:add hw-1 solution

---
+++ HW_1.sql
@@ -0,0 +1,237 @@
+create table employees(
+	id serial primary key,
+	employee_name varchar(50) not null
+);
+
+insert into employees(employee_name)
+values	('Lenette'),
+		('Dana'),
+		('Miquela'),
+		('Borg'),
+		('Mallissa'),
+		('Terrijo'),
+		('Adriaens'),
+		('Raquel'),
+		('Rurik'),
+		('Prent'),
+		('Jeffy'),
+		('Vonni'),
+		('Druci'),
+		('Raphael'),
+		('Xaviera'),
+		('Nicko'),
+		('Rebekkah'),
+		('Leanora'),
+		('Stanislaw'),
+		('Sonya'),
+		('Kassia'),
+		('Karlis'),
+		('Lynna'),
+		('Deeyn'),
+		('Robinet'),
+		('Seward'),
+		('Ki'),
+		('Culley'),
+		('Henrie'),
+		('Deb'),
+		('Egor'),
+		('Meier'),
+		('Christophorus'),
+		('Gui'),
+		('Karlens'),
+		('Tam'),
+		('Cymbre'),
+		('Barbie'),
+		('Charmain'),
+		('Dougie'),
+		('Carlotta'),
+		('Rustin'),
+		('Shermie'),
+		('Alain'),
+		('Sam'),
+		('Riobard'),
+		('Ellissa'),
+		('Arlana'),
+		('Wolfy'),
+		('Sergeant'),
+		('Angelita'),
+		('Kala'),
+		('Sallee'),
+		('Clarisse'),
+		('Silas'),
+		('Tomasina'),
+		('Cobbie'),
+		('Curr'),
+		('Enrika'),
+		('Shawn'),
+		('Leigh'),
+		('Tiff'),
+		('Truman'),
+		('Maddie'),
+		('Les'),
+		('Reinaldo'),
+		('Maighdiln'),
+		('Nancy'),
+		('Nichole'),
+		('Richmond');
+
+select * from employees;
+
+create table salary(
+	id serial primary key,
+	monthly_salary int not null
+);
+
+insert into salary(monthly_salary)
+values	(1000),
+		(1100),
+		(1200),
+		(1300),
+		(1400),
+		(1500),
+		(1600),
+		(1700),
+		(1800),
+		(1900),
+		(2000),
+		(2100),
+		(2200),
+		(2300),
+		(2400),
+		(2500);
+select * from salary;
+
+create table employee_salary(
+	id serial primary key,
+	employee_id int not null unique,
+	salary_id int not null
+);
+insert into employee_salary(id, employee_id, salary_id)
+values	(1,3,7),
+		(2,1,4),
+		(3,5,9),
+		(4,8,13),
+		(5,2,4),
+		(6,10,2),
+		(7,9,10),
+		(8,4,13),
+		(9,6,4),
+		(10,7,1),
+		(11,11,7),
+		(12,12,5),
+		(13,13,9),
+		(14,14,1),
+		(15,15,5),
+		(16,16,4),
+		(17,17,2),
+		(18,18,1),
+		(19,19,11),
+		(20,20,14),
+		(21,21,15),
+		(22,22,6),
+		(23,23,3),
+		(24,24,13),
+		(25,25,16),
+		(26,26,8),
+		(27,27,2),
+		(28,28,10),
+		(29,29,7),
+		(30,30,11),
+		(31,31,14),
+		(32,32,1),
+		(33,33,15),
+		(34,34,6),
+		(35,35,2),
+		(36,36,12),
+		(37,37,10),
+		(38,38,7),
+		(39,39,4),
+		(40,40,1);
+	
+select * from employee_salary;
+
+create table roles(
+	id serial primary key,
+	role_name int not null unique
+);
+
+alter table roles 
+alter column role_name type varchar(30);
+
+insert into roles(role_name)
+values	('Junior Python developer'),
+		('Middle Python developer'),
+		('Senior Python developer'),
+		('Junior Java developer'),
+		('Middle Java developer'),
+		('Senior Java developer'),
+		('Junior JavaScript developer'),
+		('Middle JavaScript developer'),
+		('Senior JavaScript developer'),
+		('Junior Manual QA engineer'),
+		('Middle Manual QA engineer'),
+		('Senior Manual QA engineer'),
+		('Project Manager'),
+		('Designer'),
+		('HR'),
+		('CEO'),
+		('Sales manager'),
+		('Junior Automation QA engineer'),
+		('Middle Automation QA engineer'),
+		('Senior Automation QA engineer');
+
+select * from roles;
+70,16
+
+create table roles_employee(
+	id serial primary key,
+	employee_id int not null unique,
+	role_id int not null,
+	foreign key(employee_id)
+		references employees(id),
+	foreign key (role_id)
+		references roles(id)
+);
+
+insert into roles_employee(employee_id, role_id)
+values	(5,11),
+		(8,9),
+		(3,4),
+		(9,5),
+		(1,9),
+		(4,12),
+		(7,3),
+		(2,15),
+		(21,6),
+		(26,11),
+		(24,14),
+		(28,10),
+		(22,9),
+		(41,1),
+		(49,5),
+		(43,10),
+		(45,11),
+		(40,16),
+		(50,4),
+		(44,1),
+		(51,6),
+		(53,14),
+		(59,3),
+		(55,8),
+		(58,11),
+		(54,7),
+		(60,5),
+		(68,2),
+		(63,9),
+		(65,2),
+		(66,1),
+		(13,9),
+		(15,5),
+		(19,7),
+		(11,11),
+		(17,16),
+		(14,3),
+		(12,9),
+		(33,13),
+		(36,6);
+select * from roles_employee
--- README.md
+++ README.md
@@ -1,1 +1,119 @@
-# PostgreSQL
+# HW_1
+### Таблица employees
+
+1) Создать таблицу employees
+- id. serial,  primary key,
+- employee_name. Varchar(50), not null
+2) Наполнить таблицу employee 70 строками.
+
+
+### Таблица salary
+
+3) Создать таблицу salary
+- id. Serial  primary key,
+- monthly_salary. Int, not null
+4) Наполнить таблицу salary 15 строками:
+
+|id | monthly_salary |
+|:--|:--------------:|
+|1 | 1000
+|2 | 1100
+|3 | 1200
+|4 | 1300
+|5 | 1400
+|6 | 1500
+|7 | 1600
+|8 | 1700
+|9 | 1800
+|10| 1900
+|11| 2000
+|12| 2100
+|13| 2200
+|14| 2300
+|15| 2400
+|16| 2500 
+
+
+### Таблица employee_salary
+
+5) Создать таблицу employee_salary
+- id. Serial  primary key,
+- employee_id. Int, not null, unique
+- salary_id. Int, not null
+6) Наполнить таблицу employee_salary 40 строками:
+- в 10 строк из 40 вставить несуществующие employee_id
+
+
+| id | employee_id | salary_id  |
+| :-:| :---------: | :---------:|
+|1  | 3    | 7          
+|2  | 1    | 4
+|3  | 5    |9
+|4  | 40   | 13
+|5  | 23   | 4
+|6  | 11   | 2
+|7  | 52   | 10
+|8  | 15   | 13
+|9  | 26   | 4
+|10 | 16   | 1
+|11 | 33   | 7
+|...| ...  | ...
+
+
+
+### Таблица roles
+
+7) Создать таблицу roles
+- id. Serial  primary key,
+- role_name. int, not null, unique
+8) Поменять тип столба role_name с int на varchar(30)
+9) Наполнить таблицу roles 20 строками:
+
+| id | role_name |
+| :-:| :-------: |
+|1     | Junior Python developer
+|2     | Middle Python developer
+|3     | Senior Python developer
+|4     |Junior Java developer
+|5     | Middle Java developer
+|6     | Senior Java developer
+|7     | Junior JavaScript developer
+|8     | Middle JavaScript developer
+|9     | Senior JavaScript developer
+|10    | Junior Manual QA engineer
+|11    | Middle Manual QA engineer
+|12    | Senior Manual QA engineer
+|13    | Project Manager
+|14    | Designer
+|15    | HR
+|16    | CEO
+|17    | Sales manager
+|18    | Junior Automation QA engineer
+|19    | Middle Automation QA engineer
+|20    | Senior Automation QA engineer
+
+
+
+
+### Таблица roles_employee
+
+10) Создать таблицу roles_employee
+- id. Serial  primary key,
+- employee_id. Int, not null, unique (внешний ключ для таблицы employees, поле id)
+- role_id. Int, not null (внешний ключ для таблицы roles, поле id)
+11) Наполнить таблицу roles_employee 40 строками:
+
+|id  | employee_id | role_id |
+|:---:|:-----------:|:-------:|
+|1 | 7 | 2 
+|2 | 20 | 4
+|3 |3  |9
+|4 | 5 |13
+|5 |23  |4
+|6 |11 |2
+|7 |10 |9
+|8 |22 |13
+|9 |21 |3
+|10 |34 |4
+|11 |6 |7
+|... |... |...
